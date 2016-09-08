4e9ea0c3b7e306731afac9fc3d521169cbe16b2f
Florian Gauger <flo.gauger@gmail.com>
2016-09-08 20:17:34 +0200

    Refactor javascript

--- js/main.js
+++ js/main.js
@@ -20,59 +20,66 @@ $(window).on('load', function () {
     canvas.width = sideLength;
     canvas.height = sideLength;
     var ctx = canvas.getContext('2d');
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
     var symbols = [];
+    initialize();
 
-
-    $size.on("input", draw);
-    $color_text.on("input", updateColor.bind(updateColor, $color, $color_text, false));
-    $background_color_text.on("input", updateColor.bind(updateColor, $background_color, $background_color_text, false));
-
-    $color.spectrum({
-        color: "#ffffff",
-        showButtons: false,
-        showAlpha: true,
-        move: updateColor.bind(updateColor, $color, $color_text, true)
-    });
-
-    $background_color.spectrum({
-        color: "#f85032",
-        showButtons: false,
-        showAlpha: true,
-        move: updateColor.bind(updateColor, $background_color, $background_color_text, true)
-    });
-
-    $search.on("input", function () {
-        var phrase = $search.val().toLowerCase();
-        for (var i = 0; i < symbols.length; i++) {
-            if (symbols[i].id.indexOf(phrase) !== -1) {
-                symbols[i].$icon.parent().show();
-            } else {
-                symbols[i].$icon.parent().hide();
-            }
-        }
-    });
-
-    $("#download").on("click", function () {
-        canvas.toBlob(function (blob) {
-            saveAs(blob, "favicon.png");
+    function initialize() {
+        $size.on("input", draw);
+        $color_text.on("input", updateColor.bind(updateColor, $color, $color_text, false));
+        $background_color_text.on("input", updateColor.bind(updateColor, $background_color, $background_color_text, false));
+        $color.spectrum({
+            color: "#ffffff",
+            showButtons: false,
+            showAlpha: true,
+            move: updateColor.bind(updateColor, $color, $color_text, true)
         });
-    });
 
-    $(".icon").on("click", function () {
-        symbol = window.getComputedStyle($(this).children().get()[0], ':before').content.substring(1, 2);
+        $background_color.spectrum({
+            color: "#f85032",
+            showButtons: false,
+            showAlpha: true,
+            move: updateColor.bind(updateColor, $background_color, $background_color_text, true)
+        });
+
+        $search.on("input", function () {
+            var phrase = $search.val().toLowerCase();
+            for (var i = 0; i < symbols.length; i++) {
+                if (symbols[i].id.indexOf(phrase) !== -1) {
+                    symbols[i].$icon.parent().show();
+                } else {
+                    symbols[i].$icon.parent().hide();
+                }
+            }
+        });
+
+        $("#download").on("click", function () {
+            canvas.toBlob(function (blob) {
+                saveAs(blob, "favicon.png");
+            });
+        });
+
+
+        $.each(icons, function (index, icon) {
+            var $iconOuter = $("<div>").addClass("icon_outer");
+            var $icon = $("<div>").addClass("icon").append($('<i class="fa fa-' + icon.id + '"></i>'));
+            $icon.append($("<div>").addClass("icon_text").text(icon.id));
+            $iconOuter.append($icon);
+            $right.append($iconOuter);
+
+            symbols.push(new FASymbol(icon.name, icon.id, icon.unicode, $icon));
+        });
+
+
+        $(".icon").on("click", function () {
+            symbol = window.getComputedStyle($(this).children().get()[0], ':before').content.substring(1, 2);
+            draw();
+        });
+
         draw();
-    });
+    }
 
-    $.each(icons, function (index, icon) {
-        var $iconOuter = $("<div>").addClass("icon_outer");
-        var $icon = $("<div>").addClass("icon").append($('<i class="fa fa-' + icon.id + '"></i>'));
-        $icon.append($("<div>").addClass("icon_text").text(icon.id));
-        $iconOuter.append($icon);
-        $right.append($iconOuter);
-
-        symbols.push(new FASymbol(icon.name, icon.id, icon.unicode, $icon));
-    });
-    draw();
 
     function updateColor($color, $color_text, fromPicker) {
         if (!fromPicker) {
@@ -96,11 +103,9 @@ $(window).on('load', function () {
 
     function draw() {
         if (sideLength > 0) {
-            var i = sideLength;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.fillStyle = $background_color_text.val();
             ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 
             setFontSize();
             ctx.fillStyle = "rgba(0, 0, 0, 1)";
@@ -114,8 +119,7 @@ $(window).on('load', function () {
     }
 
     function setFontSize() {
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
+        var i = sideLength;
         do {
             ctx.font = i + "px FontAwesome";
             i--;
